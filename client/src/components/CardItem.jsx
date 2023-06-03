877283694b2bb65abe0e411c41fab8b1867d50ef
dinothaurs <janiekuang8679@gmail.com>
2023-06-02 17:38:03 -0700

    Restructure cards for each individual post
    Pictures on each card spans the entire first half of post, title is the location, followed by a short description about the apartment, followed by the poster name.

--- client/src/components/CardItem.jsx
+++ client/src/components/CardItem.jsx
@@ -1,25 +1,47 @@
-import React from 'react';
+import React, {useState} from 'react';
+// import moment from 'moment';
 import { Link } from 'react-router-dom';
 
 function CardItem(props) {
   return (
-    <>
-      <li className='cards__item'>
-        <Link className='cards__item__link' to={props.path}>
-          <figure className='cards__item__pic-wrap' data-category={props.label}>
-            <img
-              className='cards__item__img'
-              alt='Travel Image'
-              src={props.src}
-            />
-          </figure>
-          <div className='cards__item__info'>
-            <h5 className='cards__item__text'>{props.text}</h5>
+    <div className='card_item'>
+      <div className='card_item_bckgrd'>
+        <div className='card_item_img'>
+          <img src={props.src} 
+          alt ={props.title}
+          className='card_item_img_inside'/>
+        </div>
+        <h1 className='heading_post'> 
+          <Link href={'/props/${props.slug}'}>
+            {props.title}
+          </Link>
+        </h1>
+        <div className='card_body'>
+          <p className='item_body'>{props.excerpt}</p>
+          <div className='card_body_inside'>
+            {/* <img 
+              alt = {props.author_name}
+              height='30px'
+              width='30px'
+              className='card-author-img'
+              src={props.author_photo}
+            /> */}
+            <p className='item_author_txt'>{props.author_name}</p>
           </div>
-        </Link>
-      </li>
-    </>
+        </div>
+        {/* <div className='item_body_center'>
+          <Link href={'/props/${props.slug}'}>
+            <span className='item_body_inside'>
+              More
+            </span>
+          </Link>
+        </div> */}
+      </div>
+    </div>
   );
-}
+} 
+// function CardItem(props) {
+
+// }
 
 export default CardItem;
--- client/src/components/Cards.jsx
+++ client/src/components/Cards.jsx
@@ -1,49 +1,46 @@
 import React from 'react';
+import Reac, { useState } from 'react';
 import CardItem from './CardItem';
 import './Cards.css';
 
 function Cards() {
   return (
     <div className='cards'>
-        <h1>Check out these EPCI retnals</h1>
-        <div className='cards__container'>
-            <div className='cards__wrapper'>
-                <ul className='cards__items'>
-                    <CardItem 
-                        src={process.env.PUBLIC_URL + './images/apartment.jpeg'}
-                        text='Pretty Apartment hehe'
-                        label='5 star'
-                        path='/services'/>
-                    <CardItem 
-                        src={process.env.PUBLIC_URL + './images/aprt3.jpeg'}
-                        text='Another one'
-                        label='4 star'
-                        path='/services'/>
-                    <CardItem 
-                        src={process.env.PUBLIC_URL + './images/aprt3.jpeg'}
-                        text='Pic 2'
-                        label='5 star'
-                        path='/services'/>
-                </ul>
-                <ul className='cards__items'>
-                    <CardItem 
-                        src={process.env.PUBLIC_URL + './images/apartment.jpeg'}
-                        text='Pretty Apartment hehe'
-                        label='5 star'
-                        path='/services'/>
-                    <CardItem 
-                        src={process.env.PUBLIC_URL + './images/aprt3.jpeg'}
-                        text='Another one'
-                        label='4 star'
-                        path='/services'/>
-                    <CardItem 
-                        src={process.env.PUBLIC_URL + './images/aprt3.jpeg'}
-                        text='Pic 2'
-                        label='5 star'
-                        path='/services'/>
-                </ul>
-            </div>
+      <h1>Check out these EPIC Destinations!</h1>
+      <div className='cards__container'>
+        <div className='cards__wrapper'>
+          <ul className='cards__items'>
+            <CardItem 
+                src={process.env.PUBLIC_URL + './images/apartment.jpeg'}
+                title='Kelton, Los Angeles'
+                author_name='Maxine'
+                author_photo={process.env.PUBLIC_URL + './images/apartment.jpeg'}
+                excerpt='This is a great apartment for you!! \nTry it'
+                />
+            <CardItem 
+                src={process.env.PUBLIC_URL + './images/aprt3.jpeg'}
+                title='Mowe, Los Angeles'
+                author_name='Janie'
+                author_photo={process.env.PUBLIC_URL + './images/apartment.jpeg'}
+                excerpt='Best apartment ever!!!!'
+                />
+            <CardItem 
+                src={process.env.PUBLIC_URL + './images/apartment.jpeg'}
+                title='ajksgd, Los Angeles'
+                author_name='Anirudhk'
+                author_photo={process.env.PUBLIC_URL + './images/apartment.jpeg'}
+                excerpt='Great spot hehe'
+                />
+            <CardItem 
+                src={process.env.PUBLIC_URL + './images/apartment.jpeg'}
+                title='agkljkfdjhuie, Los Angeles'
+                author_name='srinut'
+                author_photo={process.env.PUBLIC_URL + './images/apartment.jpeg'}
+                excerpt='WE have sooo many rooms'
+                />
+          </ul>
         </div>
+      </div>
     </div>
   );
 }
