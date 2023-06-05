6793c190814bfdd92a10fd1d07af6993d2b42d5e
maxinewu5 <maxinewu5@gmail.com>
2023-06-05 01:50:08 -0700

    Update UserProfile to use new Cards
    Update to UserProfile to new Card components. Bug Fix: Fixed a previous bug where not all the listings were appearing.

--- client/src/components/UserProfile.jsx
+++ client/src/components/UserProfile.jsx
@@ -3,6 +3,7 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth, db } from '../Firebase'
 import { collection, query, where, doc, getDoc, getDocs } from "firebase/firestore";
 import CardItem from './CardItem'
+import Cards from './Cards'
 import { getAdditionalUserInfo } from 'firebase/auth';
 
 function UserProfile() {
@@ -22,58 +23,72 @@ function UserProfile() {
         setUserData(userDataFiltered[0])
     }
 
-    const getUserProperties = async() => {
-        let userPropertiesData = []
-        userData?.properties.map(async (property) => {
-            let docRef = doc(db, "Properties", property)
-            const docSnap = await getDoc(docRef)
-            console.log(docSnap.data())
-            userPropertiesData.push(docSnap.data())
-            console.log(userPropertiesData)
-            setUserProperties(userPropertiesData)
-        })
+    //fixed code
+    const getsProperties = async () => {
         
-        //setUserProperties(userPropertiesData)
-    }
+        //gets user properties
 
+        let userPropertiesData = [];
+
+        if (userData == undefined) return
+        console.log("not null!")
+
+        await Promise.all(
+          userData.properties.map(async (property) => {
+            let docRef = doc(db, "Properties", property);
+            const docSnap = await getDoc(docRef);
+            userPropertiesData.push({...docSnap.data(), id: docRef.id});
+            //{...doc.data(), id: doc.id})
+          })
+        );
+        setUserProperties(userPropertiesData);
+
+        //gets user favorite properties
+      
+        let favPropertiesData = [];
+        await Promise.all(
+          userData.fav_properties.map(async (property) => {
+            let docRef = doc(db, "Properties", property);
+            const docSnap = await getDoc(docRef);
+            favPropertiesData.push({...docSnap.data(), id: docRef.id});
+          })
+        );
+        setFavProperties(favPropertiesData);
+    };
+
+    // const getUserProperties = async() => {
+    //     let userPropertiesData = []
+    //     userData?.properties.map(async (property) => {
+    //         let docRef = doc(db, "Properties", property)
+    //         const docSnap = await getDoc(docRef)
+    //         //console.log(docSnap.data())
+    //         userPropertiesData.push(docSnap.data())
+    //         //console.log(userPropertiesData)
+    //         setUserProperties(userPropertiesData)
+    //     })
+
+    //     let favPropertiesData = []  
+    //     console.log("user data fav property")
+    //     console.log(userData?.fav_properties)
+    //     userData?.fav_properties?.map(async (property) => {
+    //         console.log("fav property")
+    //         let docRef = doc(db, "Properties", property)
+    //         const docSnap = await getDoc(docRef)
+    //         //console.log(docSnap.data())
+    //         favPropertiesData.push(docSnap.data())
+    //         //console.log(favPropertiesData)
+    //         setFavProperties(favPropertiesData)
+    //     })
+    // }
+
+    //get properties upon user data update
     useEffect(()=> {
 
-        const fetchData = async () => {
-            let propertiesRef = collection(db, "properties")
-            const propertyData = await getDocs(propertiesRef)
-
-            //get user properties
-            //bug: small delay for the properties to actually show up, looks weird
-            getUserProperties()
-
-            //temp code: for favorite properties, needs to sync with the explore page like
-            console.log("user id:" + userData?.id)
-            if (!userData) return 
-            const q = query(propertiesRef, where("user_id", "==", userData?.id));
-            const querySnapshot = await getDocs(q);
-            querySnapshot.forEach((doc) => {console.log("doc " + doc.id, " => ", doc.data());});
-            const propFilteredData = querySnapshot.docs.map((doc) => ({...doc.data(), id: doc.id}))
-            setFavProperties(propFilteredData)
-
-            //note: additional code for processing getDocs return 
-            //const filteredData = data.docs.map((doc) => ({...doc.data(), id: doc.id}))
-        }
-
-        const executeFunctions = async () => {
-            console.log("executing funcs")
-            await fetchData()
-            console.log("userdata")
-            console.log(userData)
-        
-            console.log("fav properties")
-            console.log(favProperties)
-            console.log("finish executing funcs")
-        }
-
-        executeFunctions()
+        getsProperties()
 
     }, [ userData ])
 
+    //get user data upon load
     useEffect(()=> {
         //get user data
         //temp code: to be replaced with login auth code 
@@ -81,26 +96,29 @@ function UserProfile() {
         getUserData(email)
     }, [])
 
-    const starRating = 4.89;
+    // console.log("user properties")
+    // console.log(userProperties)   
+    // console.log("fav properties")
+    // console.log(favProperties)  
 
     return (
         <>
-            <h2>{localStorage.name} ({starRating})</h2>   
+            <h2>{localStorage.name}</h2>   
     
             <button>Add Listing</button>
             <button>Edit Profile</button>
 
-            <h3>My Listings</h3>    
-            {userProperties?.map((listing) => {
-                return <CardItem
-                    text={listing.AptName}
-                ></CardItem>
-            })}
+            <h3>My Listings</h3> 
+            
+            <Cards 
+                properties={userProperties}
+            />
 
             <h3>Favorite Listings</h3>
-            {favProperties?.map((listing) => {
-                return <CardItem></CardItem>
-            })}
+
+            <Cards 
+                properties={favProperties}
+            />
 
         </>
         );
