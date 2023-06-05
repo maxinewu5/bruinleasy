9bed9ecd420d0983f14820cd80563d47a9f4619a
dinothaurs <janiekuang8679@gmail.com>
2023-06-05 00:05:09 -0700

    Style Login page
    Style login page to have a box with half of it having a picture, other half having the login fields. Above the login fields is the logo of our website. The submit button goes from an outlined button to a filled button. The sign in with google button and create an account button change colors when hovered above.

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,7 +1,12 @@
 import React, {useState} from 'react';
+import './Login.css';
 import {Link, useNavigate} from 'react-router-dom';
 import { signInWithGoogle } from '../Firebase';
 import {getAuth, signInWithEmailAndPassword} from 'firebase/auth'
+import { Button } from './Button';
+import CreateAccount from './pages/AccountCreation';
+import { FcGoogle } from 'react-icons/fc';
+
 
 const Login = () => {
   const [EmailID, setEmailID] = useState('');
@@ -29,39 +34,49 @@ const Login = () => {
 
   return (
     <React.Fragment>
-    {/*For a form element, hitting enter on either input field will result in form submission. Classname defined for future css styling.*/}
-    <img src= {process.env.PUBLIC_URL + './images/logo.png'} alt = "BruinLeasy logo"/>
-    <form classname = "Login-Form" onSubmit={handleSubmit}> 
-    {
-      /*
-      -onChange is a pre-defined prop that defines what is to be done when changes are made to the corresponding
-      input field. 
-      -In this case, it calls a funcion defined in-line with an event object. 
-      -The event object stores the information corresponding to the change, and is automatically created whenever
-      such changes are made by the user. 
-      -One of the properties of the event is 'target' which is an object that corresponds to the specific field 
-      that was changed(i.e. the specific input field that was modified)
-      -'Value' is a property of target that stores the changes made to the input fields value. 
-       */
-    }
-    <input type = "email" placeholder = "Email id" onChange = {(event)=>{setEmailID(event.target.value)}} value = {EmailID}/>
-    <br />
-    <input type = "password" placeholder = "Password" onChange = {(event)=>{setPassword(event.target.value)}} value = {Password}/>
-    <br />
-    {
-      /*
-      -Whenever a button is defined by default within a form tag, it is automatically treated as a submit button.
-      -Clicking on this button will submit the form.
-    */
-    }
-    <button>Submit</button>
-    <br></br>
-    <br />
-    </form>
-    <button onClick={signInWithGoogle}>Sign in with Google</button>
-    <Link to='/CreateAccount'>
-    <button>New? Create An Account</button>
-    </Link>
+      <div className='login_page'>
+        <div className='login_container'>
+          <img className='login_house_logo' src={process.env.PUBLIC_URL + './images/login-key.png'} alt = "Login-House" />
+          {/*For a form element, hitting enter on either input field will result in form submission. Classname defined for future css styling.*/}
+          {/* <img src= {process.env.PUBLIC_URL + './images/logo.png'} alt = "BruinLeasy logo"/> */}
+          <div className='login_submission'>
+            <form classname = "login_form" onSubmit={handleSubmit}> 
+            {
+              /*
+              -onChange is a pre-defined prop that defines what is to be done when changes are made to the corresponding
+              input field. 
+              -In this case, it calls a funcion defined in-line with an event object. 
+              -The event object stores the information corresponding to the change, and is automatically created whenever
+              such changes are made by the user. 
+              -One of the properties of the event is 'target' which is an object that corresponds to the specific field 
+              that was changed(i.e. the specific input field that was modified)
+              -'Value' is a property of target that stores the changes made to the input fields value. 
+              */
+            }
+            <img class='login_logo_pic' src={process.env.PUBLIC_URL + './images/logo.png'} alt = "Logo" />
+            <input className='login_box' type = "email" placeholder = "Email" onChange = {(event)=>{setEmailID(event.target.value)}} value = {EmailID}/>
+            <br />
+            <input className='login_box' type = "password" placeholder = "Password" onChange = {(event)=>{setPassword(event.target.value)}} value = {Password}/>
+            <br />
+            {
+              /*
+              -Whenever a button is defined by default within a form tag, it is automatically treated as a submit button.
+              -Clicking on this button will submit the form.
+            */
+            }
+            <div className='submit'>
+            <button className='btn--outline--medium'>Submit</button>
+            </div>
+            </form>
+            <div className='login_btns'>
+              <button className='btn--outline--small' onClick={signInWithGoogle}><FcGoogle className='fcGoogle'/>   Sign in with Google</button> <br />
+              <Link to='/CreateAccount'> 
+                <button className='btn--outline--small'>New? Create An Account</button>
+              </Link>
+            </div>
+          </div>
+        </div>
+    </div>
     </React.Fragment>
   );
 }
