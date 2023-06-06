5ac9a341a5eb9fdee3a40e796248b7b94a225398
dinothaurs <janiekuang8679@gmail.com>
2023-06-06 16:31:56 -0700

    Style Occupation Page for Listings
    Style the occupation form by make the text and buttons look better and also add gifs to fill in the white space

--- client/src/components/pages/AddListings/add-occupation.jsx
+++ client/src/components/pages/AddListings/add-occupation.jsx
@@ -34,16 +34,26 @@ const AddOcc = ({ onNext, counters }) => {
           />
         <div className='listing_page'>
           <div className='listing_container'>
-          <h2>What is the apartment's occupation? </h2>
-          <div className='info_contain_amenities'>
-            <button onClick={() => decreaseCount(0)}>-</button> Bathrooms:{" "}
-            {counters[0]} <button onClick={() => increaseCount(0)}>+</button>
+          <h2 className='heading'>What is the apartment's occupation? </h2>
+          <div className='info_contain_occupation'>
+            <button className='inde' onClick={() => decreaseCount(0)}>-</button> Bathrooms:{" "}
+            {counters[0]} <button className='inde' onClick={() => increaseCount(0)}>+</button>
+          {/* </div>
+          <div className='listing_in_de'> */}
+            <div></div>
+            <button className='inde' onClick={() => decreaseCount(1)}>-</button> Bedrooms:{" "}
+            {counters[1]} <button className='inde' onClick={() => increaseCount(1)}>+</button>
           </div>
-          <div>
-            <button onClick={() => decreaseCount(1)}>-</button> Bedrooms:{" "}
-            {counters[1]} <button onClick={() => increaseCount(1)}>+</button>
-          </div>
-          <br />
+          <img
+            className='bedroom_img'
+            src={process.env.PUBLIC_URL + "./images/bedroom.gif"}
+            alt="Bedroom Image"
+          />
+          <img
+            className='bathroom_img'
+            src={process.env.PUBLIC_URL + "./images/bathroom.gif"}
+            alt="Bathroom Image"
+          />
         </div>
       </div>
     </div>
