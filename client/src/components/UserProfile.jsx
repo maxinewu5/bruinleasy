1e33a2fde295998f89c18032664cca2a3eca9135
maxinewu5 <maxinewu5@gmail.com>
2023-06-06 23:08:18 -0700

    Update to UserProfile to incorporate auth fix.
    Update to UserProfile to incorporate auth hotfix. Hopefully does not collide in merge with main.

--- client/src/components/UserProfile.jsx
+++ client/src/components/UserProfile.jsx
@@ -12,13 +12,18 @@ import {
 import CardItem from "./CardItem";
 import Cards from "./Cards";
 import { getAuth } from "firebase/auth";
-import Login from "./Login";
+import Login from "./Login"; 
 import { useNavigate } from "react-router-dom";
+import "./Button.css"
 
 function UserProfile() {
   const navigate = useNavigate();
-  const auth = getAuth();
-  const [user, loading] = useAuthState(auth);
+
+  //useAuthState hook
+  const [user, loading, error] = useAuthState(auth);
+  //not working auth code--
+  //const auth = getAuth();
+  //const user = auth.currentUser;
 
   //track state of userData, user owned properties, and fav properties
   const [userData, setUserData] = useState();
@@ -27,53 +32,76 @@ function UserProfile() {
 
   //gets user data given userEmail
   const getUserData = async (userEmail) => {
-    let usersRef = collection(db, "users");
-    const userDat = await getDocs(
-      query(usersRef, where("email", "==", userEmail))
-    );
-    const userDataFiltered = userDat.docs.map((doc) => ({
-      ...doc.data(),
-      id: doc.id,
-    }));
-    setUserData(userDataFiltered[0]);
+    let userRef = doc(db, "users", userEmail)
+    const userSnap = await getDoc(userRef)
+    const userProc = {...userSnap.data(), id:userSnap.id}
+    setUserData(userProc);
   };
+
+  // old code:
+  // const getUserData = async (userEmail) => {
+  //   let usersRef = collection(db, "users");
+  //   const userDat = await getDocs(
+  //     query(usersRef, where("email", "==", userEmail))
+  //   );
+  //   const userDataFiltered = userDat.docs.map((doc) => ({
+  //     ...doc.data(),
+  //     id: doc.id,
+  //   }));
+  //   setUserData(userDataFiltered[0]);
+  // };
 
   //fixed code
 
   //gets data for userProperties and favProperties for the user in userData
   const getsProperties = async () => {
-    //gets user properties
-    let userPropertiesData = [];
 
     //checks if the user is null
-    if (userData == undefined) return;
+    if (user == null) return; 
+
+    //get user data
+    let userRef = doc(db, "users", user.email)
+    const userSnap = await getDoc(userRef)
+    const userProc = {...userSnap.data(), id:userSnap.id}
+    setUserData(userProc)
+
+    //stores user properties
+    let userPropertiesData = []
+    //stores user favorite properties
+    let favPropertiesData = []
+
+    //if (userData == undefined) return;
 
     //use a promise to only continue when async function has finished calling
-    await Promise.all(
-      userData.properties.map(async (property) => {
-        //get the doc reference with the specified propertyid
-        let docRef = doc(db, "Properties", property);
-        //get the doc snapshot at this point in time
-        const docSnap = await getDoc(docRef);
-        //process the snapshot with .data() to pull the actual object date
-        //push to user properties
-        userPropertiesData.push({ ...docSnap.data(), id: docRef.id });
-        //note: syntax for extracting the actual data out of the firebase doc object
-        //{...doc.data(), id: doc.id})
-      })
-    );
-    setUserProperties(userPropertiesData);
+    console.log(userData)
+    if (userProc.properties != undefined) {
+      console.log(userProc?.properties)
+      await Promise.all(
+        userProc?.properties?.map(async (property) => {
+          //get the doc reference with the specified propertyid
+          let docRef = doc(db, "Properties", property);
+          //get the doc snapshot at this point in time
+          const docSnap = await getDoc(docRef);
+          //process the snapshot with .data() to pull the actual object date
+          //push to user properties
+          userPropertiesData.push({ ...docSnap.data(), id: docRef.id });
+          //note: syntax for extracting the actual data out of the firebase doc object
+          //{...doc.data(), id: doc.id})
+        })
+      )
+      setUserProperties(userPropertiesData)
+    }
 
-    //gets user favorite properties
-    let favPropertiesData = [];
-    await Promise.all(
-      userData.fav_properties.map(async (property) => {
-        let docRef = doc(db, "Properties", property);
-        const docSnap = await getDoc(docRef);
-        favPropertiesData.push({ ...docSnap.data(), id: docRef.id });
-      })
-    );
-    setFavProperties(favPropertiesData);
+    if (userProc.fav_properties != undefined) {
+      await Promise.all(
+        userProc.fav_properties?.map(async (property) => {
+          let docRef = doc(db, "Properties", property);
+          const docSnap = await getDoc(docRef);
+          favPropertiesData.push({ ...docSnap.data(), id: docRef.id });
+        })
+      )
+      setFavProperties(favPropertiesData)
+    }
   };
 
   // broken code
@@ -109,44 +137,45 @@ function UserProfile() {
   //get properties upon user data update
 
   useEffect(() => {
-    getsProperties();
-  }, [userData]);
 
-  //get user data upon load
-  useEffect(() => {
-    //#TODO: replace with login auth code
-    let email = "maxinewu5@gmail.com";
-    getUserData(email);
-  }, []);
+    getsProperties()  
 
-  if (loading) {
-    return <div></div>; // Display a loading state while authentication state is being resolved
-  }
+    //broken code:
+    // if (user != null) {
+    //   getUserData(user.email).then( ()=> {
+    //     getsProperties()  
+    //   })
+    // }
+  }, [user]);
 
-  return user ? (
+  //className="btn--outline--medium"
+
+  return (user) ? (
     <>
-      <h2>{localStorage.name}</h2>
 
-      <button
+      <br></br><br></br><br></br><br></br>
+      <img height="48px" src={user?.photoURL}></img>
+      <h3>{user?.displayName}</h3>
+  
+      <button className="btn--outline--medium"
         onClick={() => {
           navigate("/AddListing");
         }}
       >
         Add Listing
       </button>
-      <button>Edit Profile</button>
+      <button className="btn--outline--medium" >Edit Profile</button>
 
       <h3>My Listings</h3>
 
       <Cards properties={userProperties} />
-
+ 
       <h3>Favorite Listings</h3>
-
+    
       <Cards properties={favProperties} />
     </>
-  ) : (
-    <Login />
-  );
+  ) :  ( loading ? <></> : <Login />) 
 }
 
 export default UserProfile;
+
